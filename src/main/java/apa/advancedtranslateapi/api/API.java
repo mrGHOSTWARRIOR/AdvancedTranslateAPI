c17bb0cd3d3d32561754379fe34030453deb165b
mrGHOSTWARRIOR <axelgove01@gmail.com>
2023-09-17 01:49:57 -0600

    first commit

---
+++ src/main/java/apa/advancedtranslateapi/api/API.java
@@ -0,0 +1,26 @@
+package apa.advancedtranslateapi.api;
+
+
+import apa.translateapi.AdvancedTranslateAPI;
+
+public class API {
+
+
+    private final AdvancedTranslateAPI advancedTranslateAPI;
+
+
+    public API(AdvancedTranslateAPI advancedTranslateAPI) {
+        this.advancedTranslateAPI = advancedTranslateAPI;
+    }
+
+    public String translate(String text, String sourceLang, String targetLang) {
+        return advancedTranslateAPI.getTranslate().translate(text, sourceLang, targetLang);
+    }
+
+    public String getLanguage(String text) {
+        return advancedTranslateAPI.getTranslate().getLanguage(text);
+    }
+
+
+
+}
